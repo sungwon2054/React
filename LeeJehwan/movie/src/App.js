57c9cb1a839e5d70871e3b84e5456f8f04f414ad
leejehwan <wpghks7@naver.com>
2021-03-11 00:07:02 +0900

    #2.1 Reusable Components with JSX + Props

--- LeeJehwan/movie/src/App.js
+++ LeeJehwan/movie/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
-import Potato from "./Potato";
+
+function Food({ favourite }) {
+  return <h3>I love {favourite}</h3>;
+}
 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
-      <Potato />
+      <Food favourite='kimbap' />
+      <Food favourite='pizza' />
+      <Food favourite='hamburger' />
+      <Food favourite='ramen' />
     </div>
   );
 }
