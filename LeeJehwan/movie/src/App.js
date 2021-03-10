1286e5e9ed9503b5eab2cd6a0d25e5cd21a5a71a
leejehwan <wpghks7@naver.com>
2021-03-11 00:16:06 +0900

    #2.2 Dynamic Component Generation (09:54)

--- LeeJehwan/movie/src/App.js
+++ LeeJehwan/movie/src/App.js
@@ -1,17 +1,43 @@
 import React from "react";
 
-function Food({ favourite }) {
-  return <h3>I love {favourite}</h3>;
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} width='500' />
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    name: "Samgyeopsal",
+    image:
+      "https://pds.joins.com/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg",
+  },
+  {
+    name: "Bibimbap",
+    image:
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+  },
+  {
+    name: "Doncasu",
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+  },
+  {
+    name: "Kimbap",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Food favourite='kimbap' />
-      <Food favourite='pizza' />
-      <Food favourite='hamburger' />
-      <Food favourite='ramen' />
+      {foodILike.map((dish) => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
