a89349d68e22b87a6fc606da9e9adf4f45de24f9
leejehwan <wpghks7@naver.com>
2021-03-11 00:36:58 +0900

    #2.4 Protection with PropTypes

--- LeeJehwan/movie/src/App.js
+++ LeeJehwan/movie/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropType from "prop-types";
 
 const foodILike = [
   {
@@ -6,41 +7,57 @@ const foodILike = [
     name: "Samgyeopsal",
     image:
       "https://pds.joins.com/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg",
+    rating: 4.9,
   },
   {
     id: 2,
     name: "Bibimbap",
     image:
       "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+    rating: 5.0,
   },
   {
     id: 3,
     name: "Doncasu",
     image:
       "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+    rating: 3.2,
   },
   {
     id: 4,
     name: "Kimbap",
     image:
       "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+    rating: 5.4,
   },
 ];
 
-function Food({ name, picture }) {
+function Food({ name, picture, rating }) {
   return (
     <div>
       <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
       <img src={picture} alt={name} width='500' />
     </div>
   );
 }
 
+Food.PropType = {
+  name: PropType.string.isRequired,
+  picture: PropType.string.isRequired,
+  rating: PropType.number,
+};
+
 function App() {
   return (
     <div>
       {foodILike.map((dish) => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} />
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
       ))}
     </div>
   );
