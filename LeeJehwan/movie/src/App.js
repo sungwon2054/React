6e04dc236cb2df67585af5a580d03eda3dcd58fe
leejehwan <wpghks7@naver.com>
2021-03-12 01:35:16 +0900

    3.0 Class Components and State

--- LeeJehwan/movie/src/App.js
+++ LeeJehwan/movie/src/App.js
@@ -1,66 +1,24 @@
 import React from "react";
-import PropType from "prop-types";
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Samgyeopsal",
-    image:
-      "https://pds.joins.com/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg",
-    rating: 4.9,
-  },
-  {
-    id: 2,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating: 5.0,
-  },
-  {
-    id: 3,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 3.2,
-  },
-  {
-    id: 4,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 5.4,
-  },
-];
-
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} width='500' />
-    </div>
-  );
-}
-
-Food.PropType = {
-  name: PropType.string.isRequired,
-  picture: PropType.string.isRequired,
-  rating: PropType.number,
-};
-
-function App() {
-  return (
-    <div>
-      {foodILike.map((dish) => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
