c83c29e6ea6e41518f8b973578102d325481fb78
leejehwan <wpghks7@naver.com>
2021-03-16 23:18:02 +0900

    4.3 Adding Genres

--- LeeJehwan/movie/src/App.js
+++ LeeJehwan/movie/src/App.js
@@ -17,7 +17,6 @@ class App extends React.Component {
     } = await axios.get(
       "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating"
     );
-    console.log(movies);
     this.setState({ movies, isLoading: false });
   };
 
@@ -28,13 +27,13 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <section class='container'>
+      <section className='container'>
         {isLoading ? (
-          <div class='loader'>
-            <span class='loader__text'>Loading...</span>
+          <div className='loader'>
+            <span className='loader__text'>Loading...</span>
           </div>
         ) : (
-          <div class='movies'>
+          <div className='movies'>
             {movies.map((movie) => (
               <Movie
                 key={movie.id}
@@ -42,6 +41,7 @@ class App extends React.Component {
                 title={movie.title}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
+                genres={movie.genres}
               />
             ))}
           </div>
