ef3b0828aa791689522757078ad820ed3c124af1
leejehwan <wpghks7@naver.com>
2021-03-30 01:31:01 +0900

    1.3 Object Destructuring

--- LeeJehwan/netflix/chapter1/index.js
+++ LeeJehwan/netflix/chapter1/index.js
@@ -4,10 +4,31 @@ const nicolas = sayHello("Nicolas");
 console.log(nicolas);
 
 // button event with arrow function
-const button = document.querySelector("button");
-button.addEventListener("click", (event) => console.log(event));
+// const button = document.querySelector("button");
+// button.addEventListener("click", (event) => console.log(event));
 
 // 1.2 Template Literlas
 const sayHello2 = (name = "human") => `Hello ${name}`;
 const nicolas2 = sayHello2("Nicolas");
 console.log(nicolas2);
+
+// 1.3 Object Destructuring
+const human = {
+  name: "Nico",
+  lastName: "Serrano",
+  nationality: "with i was korean",
+  favFood: {
+    breakfast: "sab",
+    lunch: "doncas",
+    dinner: "sanb + doncas",
+  },
+};
+const {
+  name: diffName,
+  lastName,
+  nationality,
+  favFood: { breakfast, lunch, dinner },
+} = human;
+
+console.log(diffName, lastName, nationality);
+console.log(breakfast, lunch, dinner);
