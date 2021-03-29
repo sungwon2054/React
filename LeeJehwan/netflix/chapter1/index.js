9dbdae586a9a016001947594a5cf068f87f11b32
leejehwan <wpghks7@naver.com>
2021-03-30 01:21:17 +0900

    1.2 Template Literlas

--- LeeJehwan/netflix/chapter1/index.js
+++ LeeJehwan/netflix/chapter1/index.js
@@ -1,10 +1,13 @@
 // 1.1 arrow functions
 const sayHello = (name = "human") => "Hello " + name;
-
 const nicolas = sayHello("Nicolas");
-
 console.log(nicolas);
 
 // button event with arrow function
 const button = document.querySelector("button");
 button.addEventListener("click", (event) => console.log(event));
+
+// 1.2 Template Literlas
+const sayHello2 = (name = "human") => `Hello ${name}`;
+const nicolas2 = sayHello2("Nicolas");
+console.log(nicolas2);
