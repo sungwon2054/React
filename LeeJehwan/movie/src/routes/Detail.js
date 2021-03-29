e821a3363978182d5ad198808f1aa95cf0e3b9cb
leejehwan <wpghks7@naver.com>
2021-03-30 00:38:00 +0900

    6.5 additional page

--- LeeJehwan/movie/src/routes/Detail.js
+++ LeeJehwan/movie/src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Detail.css";
 
 class Detail extends React.Component {
   componentDidMount() {
@@ -10,11 +11,31 @@ class Detail extends React.Component {
 
   render() {
     const { location } = this.props;
-    if (location.state) {
-      return <span>{location.state.title}</span>;
-    } else {
+    if (location.state === undefined) {
       return null;
     }
+    const {
+      state: { year, title, finedSummary, poster, genres },
+    } = location;
+    return (
+      <div className='container'>
+        <div className='detail'>
+          <img src={poster} alt={title} title={title} />
+          <div className='detail__data'>
+            <h3 className='detail__title'>{title}</h3>
+            <h5 className='detail__year'>{year}</h5>
+            <ul className='detail__genres'>
+              {genres.map((genre, index) => (
+                <li key={index} className='genres__genre'>
+                  {genre}
+                </li>
+              ))}
+            </ul>
+            <p className='detail__summary'>{finedSummary}...</p>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
