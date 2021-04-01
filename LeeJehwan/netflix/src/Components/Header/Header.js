30def657424729c33d91f73ee181c2230d09f80c
leejehwan <wpghks7@naver.com>
2021-04-02 01:51:27 +0900

    3.0 CSS in React part One

--- LeeJehwan/netflix/src/Components/Header/Header.js
+++ LeeJehwan/netflix/src/Components/Header/Header.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Header.css";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => (
-  <header>
+  <header className='nav'>
     <ul>
       <li>
         <a href='/'>Movies</a>
