0dcbb3dcdfc45ac3b2695fddf1a9ffd235ba01f4
unknown <chsw93@gmail.com>
2021-05-09 21:55:32 +0900

    2.1 React Router Part One

--- JeongSungwon/netfilx/src/App.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-function App() {
-  return (
-    <div className="App" />
-  );
-}
-
-export default App;
---
+++ JeongSungwon/netfilx/정리/Chapter2.md
@@ -0,0 +1,3 @@
+# 2.1 React Router Part One
+
+.env에 "NODE_PATH=src"로 인해 src폴더가 기본 폴더로 된다.
