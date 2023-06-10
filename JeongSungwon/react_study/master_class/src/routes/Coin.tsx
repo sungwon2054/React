ea249bb88fccdab068d0dd2f3733997568e3a954
sungwon2054 <chsw93@gmail.com>
2023-06-10 15:13:44 +0900

    6.1 Dark Mode part Two

--- JeongSungwon/react_study/master_class/src/routes/Coin.tsx
+++ JeongSungwon/react_study/master_class/src/routes/Coin.tsx
@@ -145,7 +145,11 @@ interface PriceData {
   };
 }
 
-function Coin() {
+interface ICoinProps {
+  isDark: boolean;
+}
+
+function Coin({isDark}: ICoinProps) {
   const { coinId } = useParams<RouteParams>();
   const { state } = useLocation<RouteState>();
   const priceMatch = useRouteMatch("/:coinId/price");
@@ -212,7 +216,7 @@ function Coin() {
               <Price />
             </Route>
             <Route path={`/:coinId/chart`}>
-              <Chart coinId={coinId}/>
+              <Chart isDark={isDark} coinId={coinId}/>
             </Route>
           </Switch>
         </>
