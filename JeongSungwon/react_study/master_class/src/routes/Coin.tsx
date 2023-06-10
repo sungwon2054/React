06c6aebe88e2beb6b443f97f3e7160575e9c391d
sungwon2054 <chsw93@gmail.com>
2023-06-10 15:19:30 +0900

    6.2 Introduction to Recoil

--- JeongSungwon/react_study/master_class/src/routes/Coin.tsx
+++ JeongSungwon/react_study/master_class/src/routes/Coin.tsx
@@ -145,11 +145,9 @@ interface PriceData {
   };
 }
 
-interface ICoinProps {
-  isDark: boolean;
-}
+interface ICoinProps {}
 
-function Coin({isDark}: ICoinProps) {
+function Coin({}: ICoinProps) {
   const { coinId } = useParams<RouteParams>();
   const { state } = useLocation<RouteState>();
   const priceMatch = useRouteMatch("/:coinId/price");
@@ -216,7 +214,7 @@ function Coin({isDark}: ICoinProps) {
               <Price />
             </Route>
             <Route path={`/:coinId/chart`}>
-              <Chart isDark={isDark} coinId={coinId}/>
+              <Chart coinId={coinId}/>
             </Route>
           </Switch>
         </>
