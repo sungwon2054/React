7d1292e50f0596bcaad19a85b57f9ed792f01be5
sungwon2054 <chsw93@gmail.com>
2025-06-14 17:39:42 +0900

    5.12 Price Chart

--- JeongSungwon/react-masterclass/src/api.ts
+++ JeongSungwon/react-masterclass/src/api.ts
@@ -1,4 +1,5 @@
 const BASE_URL = `https://api.coinpaprika.com/v1`;
+const OHLCV_URL = `https://ohlcv-api.nomadcoders.workers.dev`;
 
 export function fetchCoins() {
   return fetch(`${BASE_URL}/coins`).then((response) => response.json());
@@ -14,4 +15,12 @@ export function fetchCoinTickers(coinId: string) {
   return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) =>
     response.json()
   );
+}
+
+export function fetchCoinHistory(coinId: string) {
+  const endDate = Math.floor(Date.now() / 1000);
+  const startDate = endDate - 60 * 60 * 24 * 7 * 2;
+  return fetch(
+    `${OHLCV_URL}/?coinId=${coinId}`
+  ).then((response) => response.json());
 }
--- JeongSungwon/react-masterclass/src/routes/Coin.tsx
+++ JeongSungwon/react-masterclass/src/routes/Coin.tsx
@@ -205,7 +205,7 @@ function Coin() {
               <Price />
             </Route>
             <Route path={`/:coinId/chart`}>
-              <Chart />
+              <Chart coinId={coinId}/>
             </Route>
           </Switch>
         </>
