b655105afa4c4c73a690528196c640035e8c50ea
sungwon2054 <chsw93@gmail.com>
2025-06-14 16:03:36 +0900

    5.9 React Query part One

--- JeongSungwon/react-masterclass/src/routes/Coin.tsx
+++ JeongSungwon/react-masterclass/src/routes/Coin.tsx
@@ -45,6 +45,7 @@ const OverviewItem = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 33%;
 
   span:first-child {
     font-size: 10px;
@@ -76,6 +77,7 @@ const Tab = styled.span<{ isActive: boolean }>`
     props.isActive ? props.theme.accentColor : props.theme.textColor};
   a {
     display: block;
+    padding: 7px 0px;
   }
 `;
 
